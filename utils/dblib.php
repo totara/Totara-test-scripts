466766f5e51813b18894067da16c5ceef333aa45
Simon Coggins <simon.coggins@totaralms.com>
2013-09-06 13:56:23 +1200

    utils: Added loaddb and savedb scripts to backup/restore a site database

--- utils/dblib.php
+++ utils/dblib.php
@@ -14,12 +14,12 @@ class mydb {
         if (!class_exists($classname)) {
             throw new Exception("Invalid dbtype '{$dbtype}'\n");
         }
-
         return new $classname($dbhost, $dbuser, $dbpass, $dbname);
     }
 }
 
 class mydb_pgsql extends mydb {
+    private $_dbhost, $_dbname, $_dbuser, $_dbpass;
     /**
      * Create a postgres database connection
      *
@@ -36,6 +36,11 @@ class mydb_pgsql extends mydb {
         if (!$this->_conn) {
             throw new Exception("Failed to connect to PgSQL: " . pg_last_error($this->_conn));
         }
+
+        $this->_dbhost = $dbhost;
+        $this->_dbuser = $dbuser;
+        $this->_dbpass = $dbpass;
+        $this->_dbname = $dbname;
     }
 
     /**
@@ -100,9 +105,84 @@ class mydb_pgsql extends mydb {
         $sql = "CREATE DATABASE \"{$dbname}\" ENCODING = 'UTF8'";
         $this->query($sql);
     }
+
+    /**
+     * Backup a database. Will fail by returning false if the database
+     * doesn't exist.
+     *
+     * @param string $dbname Name of the database to backup.
+     * @param string $filename Path to a file to store the backup.
+     * @return bool True on success.
+     */
+    public function backupdb($dbname, $filename) {
+        // make sure the database exists
+        $sql = "SELECT * FROM pg_catalog.pg_database WHERE datname = '{$dbname}'";
+        $result = $this->query($sql);
+        if (empty($result)) {
+            // database doesn't exist
+            return false;
+        }
+
+        $dbhost = escapeshellarg($this->_dbhost);
+        $dbname = escapeshellarg($dbname);
+        $dbuser = escapeshellarg($this->_dbuser);
+        // Handle empty password.
+        if (empty($this->_dbpass)) {
+            $dbpasscmd = '';
+        } else {
+            $dbpasscmd = '-W ' . escapeshellarg($this->_dbpass);
+        }
+
+        // Backup to $filename
+        $command = "pg_dump -h {$dbhost} -U {$dbuser} {$dbpasscmd} -O -Fc {$dbname} > {$filename}";
+        exec($command, $output, $returncode);
+
+        // 0 means success.
+        return ($returncode == 0);
+    }
+
+    /**
+     * Restore a database. Will drop any existing database called $dbname
+     * to make way for the new copy. Will fail by returning false if
+     * restore fails.
+     *
+     * @param string $dbname Name of the database to restore to.
+     * @param string $filename Name of the file containing the archived db.
+     * @return bool True on success.
+     */
+    public function restoredb($dbname, $filename) {
+
+        // See if the database exists
+        $sql = "SELECT * FROM pg_catalog.pg_database WHERE datname = '{$dbname}'";
+        $result = $this->query($sql);
+        if (!empty($result)) {
+            // Drop it if it exists.
+            $this->dropdb($dbname);
+        }
+        // Create empty database
+        $this->createdb($dbname);
+
+        $dbhost = escapeshellarg($this->_dbhost);
+        $dbname = escapeshellarg($dbname);
+        $dbuser = escapeshellarg($this->_dbuser);
+        // Handle empty password.
+        if (empty($this->_dbpass)) {
+            $dbpasscmd = '';
+        } else {
+            $dbpasscmd = '-W ' . escapeshellarg($this->_dbpass);
+        }
+
+        $command = "pg_restore -O -Fc -h {$dbhost} -U {$dbuser} {$dbpasscmd} -d {$dbname} $filename";
+        exec($command, $output, $returncode);
+
+        // 0 means success.
+        return ($returncode == 0);
+    }
 }
 
 class mydb_mysql extends mydb {
+    private $_dbhost, $_dbname, $_dbuser, $_dbpass;
+
     /**
      * Create a mysql database connection
      *
@@ -118,6 +198,11 @@ class mydb_mysql extends mydb {
         if ($this->_conn->connect_error) {
             throw new Exception("Failed to connect to MySQL: " . $this->_conn->connect_error);
         }
+
+        $this->_dbhost = $dbhost;
+        $this->_dbuser = $dbuser;
+        $this->_dbpass = $dbpass;
+        $this->_dbname = $dbname;
     }
 
     /**
@@ -181,6 +266,79 @@ class mydb_mysql extends mydb {
     public function createdb($dbname) {
         $sql = "CREATE DATABASE `{$dbname}` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci";
         $this->query($sql);
+    }
+
+    /**
+     * Backup a database. Will fail by returning false if the database
+     * doesn't exist.
+     *
+     * @param string $dbname Name of the database to backup.
+     * @param string $filename Path to a file to store the backup.
+     * @return bool True on success.
+     */
+    public function backupdb($dbname, $filename) {
+        $sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{$dbname}'";
+        $result = $this->query($sql);
+        if (empty($result)) {
+            // database doesn't exist
+            return false;
+        }
+
+        $dbhost = escapeshellarg($this->_dbhost);
+        $dbname = escapeshellarg($dbname);
+        $dbuser = escapeshellarg($this->_dbuser);
+        // Handle empty password.
+        if (empty($this->_dbpass)) {
+            $dbpasscmd = '';
+        } else {
+            $dbpasscmd = '-p ' . escapeshellarg($this->_dbpass);
+        }
+
+        // Backup to $filename
+        $command = "mysqldump -h {$dbhost} -u {$dbuser} {$dbpasscmd} {$dbname} > {$filename}";
+        exec($command, $output, $returncode);
+
+        // 0 means success.
+        return ($returncode == 0);
+    }
+
+    /**
+     * Restore a database. Will drop any existing database called $dbname
+     * to make way for the new copy. Will fail by returning false if
+     * restore fails.
+     *
+     * @param string $dbname Name of the database to restore to.
+     * @param string $filename Name of the file containing the archived db.
+     * @return bool True on success.
+     */
+    public function restoredb($dbname, $filename) {
+        // See if database exists.
+        $sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{$dbname}'";
+        $result = $this->query($sql);
+        if (!empty($result)) {
+            // Drop it if it exists.
+            $this->dropdb($dbname);
+        }
+        // Create empty database
+        $this->createdb($dbname);
+
+        $dbhost = escapeshellarg($this->_dbhost);
+        $dbname = escapeshellarg($dbname);
+        $dbuser = escapeshellarg($this->_dbuser);
+        // Handle empty password.
+        if (empty($this->_dbpass)) {
+            $dbpasscmd = '';
+        } else {
+            $dbpasscmd = '-p ' . escapeshellarg($this->_dbpass);
+        }
+
+        // Backup to $filename
+        $command = "mysql -h {$dbhost} -u {$dbuser} {$dbpasscmd} {$dbname} < {$filename}";
+        exec($command, $output, $returncode);
+
+        // 0 means success.
+        return ($returncode == 0);
+
     }
 }
 
