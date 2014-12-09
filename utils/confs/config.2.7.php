1d9c3be34bce5f58e77e51ea0b9c2f3ada40fbb8
Simon Coggins <simon.coggins@totaralms.com>
2014-12-10 09:04:56 +1300

    Add behat wwwroot and 2.7 config file

---
+++ utils/confs/config.2.7.php
@@ -0,0 +1,52 @@
+<?php  // Totara 2.5 configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = '%%dbtype%%';
+$CFG->dblibrary = '%%dblibrary%%';
+$CFG->dbhost    = '%%dbhost%%';
+$CFG->dbname    = '%%dbname%%';
+$CFG->dbuser    = '%%dbuser%%';
+$CFG->dbpass    = '%%dbpass%%';
+$CFG->prefix    = '%%prefix%%';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbsocket' => 0,
+);
+
+$CFG->dirroot   = '%%dirroot%%';
+$CFG->wwwroot   = '%%wwwroot%%';
+$CFG->dataroot  = '%%dataroot%%';
+$CFG->admin     = 'admin';
+$CFG->passwordsaltmain = '';
+
+$CFG->directorypermissions = 0777;
+
+$CFG->phpunit_prefix = 'tst_';
+$CFG->phpunit_dataroot = '%%phpunit_dataroot%%';
+$CFG->behat_prefix = 'bht_';
+$CFG->behat_dataroot = '%%behat_dataroot%%';
+$CFG->behat_wwwroot = '%%behat_wwwroot%%';
+
+$CFG->debug = E_ALL | E_STRICT;
+$CFG->debugdisplay = 1;
+$CFG->perfdebug = 15;
+
+$CFG->cachejs = false;
+$CFG->langstringcache = false;
+$CFG->themedesignermode = true;
+
+$CFG->passwordpolicy = false;
+$CFG->defaultcity = '%%defaultcity%%';
+$CFG->country = '%%defaultcountry%%';
+
+// Some optional useful settings
+//$CFG->noemailever = true;
+//$CFG->divertallemailsto = 'simon.coggins@totaralms.com';
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
--- utils/settings-dist.php
+++ utils/settings-dist.php
@@ -21,6 +21,7 @@ $settings->wwwroot = 'http://localhost/%%instance%%';
 $settings->dataroot = '/path/to/moodledata/%%instance%%';
 $settings->phpunit_dataroot = '/path/to/phpunitdata/%%instance%%';
 $settings->behat_dataroot = '/path/to/behatdata/%%instance%%';
+$settings->behat_wwwroot = 'http://localhost:8000';
 
 // Backup directory path. Used by savedb/loaddb to for DB and data backups.
 $backupdir = '/tmp/backup';
